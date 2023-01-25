df7bbcb148e94cfae67aa46cdf4b058fcab35cd9
jplanckeel <jplanckeel@scaleway.com>
2023-01-25 14:07:44 +0100

    feat(js): init chrome extension

---
+++ popup.js
@@ -0,0 +1,20 @@
+console.log('<----- Extension script started running ----->');
+
+document.querySelector('.buttons').addEventListener('click', function () {
+   
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var current = tabs[0];
+        url = current.url;
+        title = current.title;
+        extractTitle = title.match(/(.*)\(!(\d+)\).*Merge requests \W(.*)\W Gitlab/i)
+        console.log(extractTitle)
+        mrTitle= extractTitle[1]
+        mrId= extractTitle[2]
+        mrProject= extractTitle[3].replace(/\s/g, '')
+
+        reviewMessage = ":please_review: `" + mrProject + "` " + mrTitle + " [MR" + mrId +"](" + url + ")"    
+        console.log(reviewMessage)
+        navigator.clipboard.writeText(reviewMessage);
+    });
+
+});
